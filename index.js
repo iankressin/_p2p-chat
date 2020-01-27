b4728529b82c7b3675ce6db71ab91c25ca8167aa
Ian <ianguimaraes31@gmail.com>
2020-01-26 23:00:48 -0300

    Propagating message between peers

--- index.js
+++ index.js
@@ -4,49 +4,22 @@ const streamSet = require("stream-set");
 const me = process.argv[2];
 const friend = process.argv[3];
 
+let con;
+
 const activePeers = streamSet();
 
-const peer = topology(me, friend);
+const peer = topology(me, [friend]);
 
-// TODO: share activePeers with new connection
-// eu PRECISO ter pelo menos uma conexao
-peer.on("connection", (connection, p) => {
+peer.on("connection", (connection, peer) => {
   activePeers.add(connection);
+  connection.on("data", data => {
+    console.log("Data: ");
+  });
 });
 
 process.stdin.on("data", data => {
   const msg = data.toString().trim();
-  activePeers.streams.forEach(connection => {
+  activePeers.forEach(connection => {
     connection.write(msg);
   });
 });
-
-// const peer
-
-// meCon.on("connection", (connection, peer) => {
-//   console.log(`${me} connected to peer ${peer}`);
-// });
-
-// friendCon.on("connection", (connection, peer) => {
-//   console.log(`${friend} connected to peer ${peer}`);
-// });
-
-// const t1Addr = "127.0.0.1:4001";
-// const t2Addr = "127.0.0.1:4002";
-// const t3Addr = "127.0.0.1:4003";
-
-// const t1 = topology(t1Addr, [t2Addr, t3Addr]);
-// const t2 = topology(t2Addr, [t1Addr, t3Addr]);
-// const t3 = topology(t3Addr, [t1Addr, t2Addr]);
-
-// t1.on("connection", (connection, peer) => {
-//   console.log(`t1 is connected to ${peer}`);
-// });
-
-// t2.on("connection", (connection, peer) => {
-//   console.log(`t2 is connected to ${peer}`);
-// });
-
-// t3.on("connection", (connection, peer) => {
-//   console.log(`t3 is connected to ${peer}`);
-// });
